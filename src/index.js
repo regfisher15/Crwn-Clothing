b60c8dd79fe68414eec968b4f5d023a33d8d02bc
rbfisher15 <rbfisher@crimson.ua.edu>
2024-09-30 12:34:36 -0500

    Create cart dropdown and toggle functionality for the dropdown

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
-import { ProductsProvider } from './contexts/products.context'
+import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
 import reportWebVitals from './reportWebVitals';
 
 
@@ -15,7 +16,9 @@ root.render(
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
